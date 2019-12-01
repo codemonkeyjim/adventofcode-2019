ea324f69bcc37a8a2b2bb26294468907b921258a
Jim Pfleger <jpfleger@acm.org>
2019-12-01 00:28:58 -0700

    Day 1-1 done

---
+++ day1-1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from math import trunc
+
+
+def fuel_needed(mass: int) -> int:
+    return trunc(mass / 3) - 2
+
+
+if __name__ == '__main__':
+    with open('data/day1-1.txt') as data:
+        total = sum([fuel_needed(int(line)) for line in data])
+    print(total)
